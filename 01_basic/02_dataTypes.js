d75d992915084b3198dacfa06fbc00d0a1ed6d50
mayur2605 <57018500+mayur2605@users.noreply.github.com>
2025-07-05 07:37:26 +0000

    data typpes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+
+// alert(3 + 3 ) we are using nodejs, not browser
+
+
+console.log(3 + 3)
+console.log("mayur")
+
+
+let name = "mayur"
+let age = 25
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//Boolean  => true/false
+//null=> standalone value
+//undefined => value not assigned 
+//symbole => unique
+
+
+//object
+
+console.log(typeof age);
