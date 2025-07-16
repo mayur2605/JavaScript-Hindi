fbed24b552ac9fb2be57abcb73520e4f78dbef23
mayur2605 <57018500+mayur2605@users.noreply.github.com>
2025-07-16 10:02:15 +0000

    all

--- part4/loopChallengs.js
+++ part4/loopChallengs.js
@@ -1,40 +1,57 @@
-// let sum = 0;
-// let i = 1;
-// while (i <= 5) {
-//   sum = sum + i;
-//   i++
+// // let sum = 0;
+// // let i = 1;
+// // while (i <= 5) {
+// //   sum = sum + i;
+// //   i++
   
-// }
+// // }
 
-// console.log(sum);
+// // console.log(sum);
 
-// let countdowsn = [];
+// // let countdowsn = [];
 
-// let i = 5;
+// // let i = 5;
 
-// while (i > 0) {
-//   countdowsn.push(i);
-//   i--;
-// }
+// // while (i > 0) {
+// //   countdowsn.push(i);
+// //   i--;
+// // }
 
-// console.log(countdowsn);
+// // console.log(countdowsn);
 
-let teaCollection = [];
-let tea;
+// let teaCollection = [];
+// let tea;
 
-do {
-  tea = prompt(`"Enter your febourite tea "`)
+// do {
+//   tea = prompt(`"Enter your febourite tea "`)
 
-  if (tea !== "stop") {
-    teaCollection.push(tea)
-  }
-} while (tea != "stop");
+//   if (tea !== "stop") {
+//     teaCollection.push(tea)
+//   }
+// } while (tea != "stop");
 
 
-let multipliedNumber = [];
-let numbers = [2, 4, 6]
+// let multipliedNumber = [];
+// let numbers = [2, 4, 6]
 
-for (let i = 0; i < numbers.length; i++) {
+// for (let i = 0; i < numbers.length; i++) {
  
 
-}
+// }
+
+
+
+let countdown = [];
+
+let j = 5;
+while (j > 0) {
+  countdown.push(j);
+  j--
+};
+// console.log(countdown);
+
+
+let teaCollection = []
+
+let tea;
+
---
+++ part4/loopnew.js
@@ -0,0 +1,7 @@
+let teas = ["green tea", "black tea", "chai", "oolong tea"];
+let selectedTeas = [];
+
+for (let i =0; i < teas.length i++) {
+  const element = ari];
+  
+}
